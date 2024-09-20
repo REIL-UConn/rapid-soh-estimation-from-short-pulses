b413e330a5b5e4d0e6d25c7bf6be9333b28de3fd
Ben Nowacki <benjamin.nowacki@uconn.edu>
2024-09-20 14:24:24 -0400

    Added preprocessed data loading functions

--- rapid_soh_estimation/rapid_soh_estimation/common_methods.py
+++ rapid_soh_estimation/rapid_soh_estimation/common_methods.py
@@ -56,5 +56,43 @@ def clean_time_series_features(ts, ys):
     return ts_clean, ys_clean
 
 
+
+def get_preprocessed_data_files(data_type:str, cell_id:int):
+	'''Returns a list of Path objects to all pkl files containing data for this cell
+	data_type : {'rpt', 'cycling'}
+	\tWether to look for RPT or Cycling data
+	cell_id : int 
+	'''
+
+	assert data_type in ['rpt', 'cycling']
+
+	dir_data = dir_processed_data.joinpath(f'{data_type}_data')
+	all_files = list(dir_data.glob(f'{data_type}_cell_{cell_id:02d}*'))
+
+	def _file_part(file_path:Path):
+		file_str = str(file_path.name)
+		return int(file_str[file_str.rindex('_part') + len('_part') : file_str.rindex('.pkl')])
+	if '_part' in str(all_files[0]):
+		return sorted(all_files, key=_file_part)
+	else:
+		assert len(all_files) == 1
+		return all_files[0]
+	
+def load_processed_data(file_paths) -> pd.DataFrame:
+	'''Loads the processed data contained at the provided file path(s)
+	file_paths : Path or list of Path objects
+	\tIf multiple file paths are provided, the data will be concatenated into a single dataframe
+	'''
+
+	if hasattr(file_paths, '__len__'):
+		all_data = []
+		for file_path in file_paths:
+			all_data.append( pickle.load(open(file_path, 'rb')) )
+		return pd.concat(all_data, ignore_index=True)
+	else:
+		return pickle.load(open(file_paths, 'rb'))
+
+
+
 if __name__ == '__main__':
     print('common_methods.py')
